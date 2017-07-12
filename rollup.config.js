427420ed27443d8e8c992515093933062c2c060a
Filipe Costa <filipebarcos@gmail.com>
2017-07-12 13:38:18 -0300

    Fixing rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,42 @@
 /* eslint-disable */
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import inject from 'rollup-plugin-inject'
-import json from 'rollup-plugin-json'
-import nodeResolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
-import uglify from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import inject from 'rollup-plugin-inject';
+import json from 'rollup-plugin-json';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 
-const processShim = '\0process-shim'
+const processShim = '\0process-shim';
 
-const prod = process.env.PRODUCTION
-const env = prod ? 'production' : 'development'
+const prod = process.env.PRODUCTION;
+const env = prod ? 'production' : 'development';
 
-console.log(`Creating ${env} bundle...`)
+console.log(`Creating ${env} bundle...`);
 
-const targets = prod ?
-  [
-    { dest: 'dist/hedron.min.js', format: 'umd' },
-  ] :
-  [
-    { dest: 'dist/hedron.js', format: 'umd' },
-    { dest: 'dist/hedron.es.js', format: 'es' },
-  ]
+const targets = prod
+  ? [{ dest: 'dist/hedron-glamorous.min.js', format: 'umd' }]
+  : [
+      { dest: 'dist/hedron-glamorous.js', format: 'umd' },
+      { dest: 'dist/hedron-glamorous.es.js', format: 'es' },
+    ];
 
 const plugins = [
   {
     resolveId(importee) {
-      if (importee === processShim) return importee
-      return null
+      if (importee === processShim) return importee;
+      return null;
     },
     load(id) {
-      if (id === processShim) return 'export default { argv: [], env: {} }'
-      return null
+      if (id === processShim) return 'export default { argv: [], env: {} }';
+      return null;
     },
   },
   babel({
     exclude: 'node_modules/**',
     babelrc: false,
-    presets: [
-      'es2015-rollup',
-      'react',
-      'stage-2'
-    ],
-    plugins: [
-      'array-includes',
-      'external-helpers',
-      'flow-react-proptypes'
-    ],
+    presets: ['es2015-rollup', 'react', 'stage-2'],
+    plugins: ['array-includes', 'external-helpers', 'flow-react-proptypes'],
   }),
   commonjs(),
   inject({
@@ -57,17 +47,16 @@ const plugins = [
   replace({
     'process.env.NODE_ENV': JSON.stringify(prod ? 'production' : 'development'),
   }),
-]
+];
 
-if (prod) plugins.push(uglify())
+if (prod) plugins.push(uglify());
 
 export default {
   entry: 'src/index.js',
   exports: 'named',
-  external: ['react', 'styled-components'],
-  globals: { react: 'React', 'styled-components': 'styled' },
-  moduleName: 'hedron',
+  external: ['react', 'glamorous', 'glamor'],
+  globals: { react: 'React', glamorous: 'glamorous', glamor: 'Glamor' },
+  moduleName: 'hedron-glamorous',
   plugins,
   targets,
-}
-
+};
